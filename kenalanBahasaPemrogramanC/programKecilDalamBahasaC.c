d38a4edff05fbb74c1353e63f6750e109c97ba27
Dimitri Irfan <dimitriirfan08@gmail.com>
2023-01-01 14:21:45 +0800

    Sedikit menambahkan materi pemrograman tanpa kemabalian nilai

--- kenalanBahasaPemrogramanC/programKecilDalamBahasaC.c
+++ kenalanBahasaPemrogramanC/programKecilDalamBahasaC.c
@@ -8,5 +8,22 @@ program utama terdiri dari dua bagian, yaitu=
 1. Kamus yang berisi nama-nama yang dideklarasi, selanjutnya dapat diacu untuk mengambil nilainya
 2. Program, yaitu satu atau deretan instruksi yang ditulis untuk dikerjakan
 */
+/*
 // setiap bahasa c, baik dideklarasi maupun instruksi, selalu diakhiri dengan(;)
 // atau diakhiri dengan suatu blok yang terdiri dituliskan dianta {dan}
+*/
+
+/*program tanpa kembalian nilai
+contoh program sederhana adalah Halo Dunia, atau lebih sering kita kenal dengan Hello World!
+karena program tidak mengirimkan nilai apapun, hanya menuliskan sebuah string
+*/
+/*
+// badan terdiri dari satu instruksi menulis yaitu printf
+// jika menulis, memerlukan library standar untuk baca/tulis sering dirulis dengan #include<stdio.h>
+*/
+#include <stdio.h>
+int main(void)
+{
+    /* code */
+    printf("Hello World!");
+}
