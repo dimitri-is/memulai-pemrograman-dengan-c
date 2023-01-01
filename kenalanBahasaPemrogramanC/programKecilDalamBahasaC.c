833419e462d6de86f3b5357fd11a2bf780ade72b
Dimitri Irfan <dimitriirfan08@gmail.com>
2023-01-01 13:14:28 +0800

    Memberikan sedikit informasi materi tentang Pengantar program kecil dalam bahasa c

---
+++ kenalanBahasaPemrogramanC/programKecilDalamBahasaC.c
@@ -0,0 +1,12 @@
+/*
+teks program bahasa c terbagi dua bagian
+1. Teks yang diproses oleh Compiler
+2. Teks yang tidak diproses oleh compiler, yaitu yang ditulis di antara seperti materi yang terdapat pada komentar ini. Ini merupakan contoh komentar yang memudahkan kita memahami sebuah program  */
+// kita dapat menyimpan file bahasa c dengan menggunakan ekstensi (.c)
+/*
+program utama terdiri dari dua bagian, yaitu=
+1. Kamus yang berisi nama-nama yang dideklarasi, selanjutnya dapat diacu untuk mengambil nilainya
+2. Program, yaitu satu atau deretan instruksi yang ditulis untuk dikerjakan
+*/
+// setiap bahasa c, baik dideklarasi maupun instruksi, selalu diakhiri dengan(;)
+// atau diakhiri dengan suatu blok yang terdiri dituliskan dianta {dan}
