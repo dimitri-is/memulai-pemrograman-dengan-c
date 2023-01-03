5dc18ae642f1b545d6cfa76e8a6c1b882d6499e2
Dimitri Irfan <dimitriirfan08@gmail.com>
2023-01-03 18:04:03 +0800

    Telah memberikan code sintaks default untuk menuliskan program awal pada bahasa c

--- kenalanBahasaPemrogramanC/dirListProgramCodeBlocksDevCPP/programKecil1.c
+++ kenalanBahasaPemrogramanC/dirListProgramCodeBlocksDevCPP/programKecil1.c
@@ -1,1 +1,10 @@
+/*ini contoh komentar*/
 
+/*ini penempatan header*/
+#include<stdio.h>
+int
+main(void)
+{ /*ini badan program*/
+    printf("Hello World!\n"); /*menambahkan simbol \n berguna untuk memberikan rasa enter pada baris program yang dipanggil*/
+    return.0; /* berfungsi untuk mengirimkan nilai pengembalian, yaitu nilai 0*/
+}
